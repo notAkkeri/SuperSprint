d15e8296561c85f171a1b5367b084b048bb81e96
daScuderiaSha <shahid.anjum.shaikh@gmail.com>
2023-08-03 00:05:38 +0100

    Basic logic & Framework
    Added asset file allocation.
    Made beginning screen in pygame.

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame, sys
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280,720))
+        pygame.display.set_caption("Super Sprint")
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+          for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            pygame.display.update()
+        
+if __name__ == "__main__":
+   game = Game()
+   game.run()
+        
